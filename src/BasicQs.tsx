e4cab38dd56ff900ad743dcc30efb7dfc2ab0279
Luc De Nardi <denardiluc@gmail.com>
2025-03-21 08:22:20 -0400

    turned both into a button and have them to right side

--- src/BasicQs.tsx
+++ src/BasicQs.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 function GoHomeScreen() {
     return(
         <div>
             <h5>
-                <Link to="/">Homepage</Link>
+                <Link to="/">
+                    <Button style={{float:'right'}}>
+                        Homepage
+                    </Button></Link>
             </h5>
         </div>
     );
--- src/DetailedQs.tsx
+++ src/DetailedQs.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 function GoHomeScreen() {
     return(
-        <div>
-            <h5>
-                <Link to="/">Homepage</Link>
-            </h5>
+        <div style={{ display: "flex" }}>
+            <header>
+                <Link to="/">
+                    <Button style={{float:'right'}}>
+                        Homepage
+                    </Button>
+                </Link>
+            </header>
         </div>
     );
 }
