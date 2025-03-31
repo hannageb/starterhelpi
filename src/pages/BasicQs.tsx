d61e49321ef1af67c35bb0db88d3aaca65d6fd91
Luc De Nardi <denardiluc@gmail.com>
2025-03-31 12:49:27 -0400

    added skeleton for questions

--- src/pages/BasicQs.tsx
+++ src/pages/BasicQs.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
 
 export function GoHomeScreen() {
     const [goToHome, setGoToHome] = React.useState(false)
@@ -21,9 +22,28 @@ export function GoHomeScreen() {
     );
 }
 function BasicQ(){
+    const [response, setResponse] = useState<string>("");
+
+    function updateResponse(event: React.ChangeEvent<HTMLInputElement>) {
+        setResponse(event.target.value);
+    }
     return(
         <div>
-            <GoHomeScreen/>
+        <GoHomeScreen></GoHomeScreen>
+        <div>
+            <h3 style={{alignItems:'center'}}>
+                Question filler    
+            </h3>
+            <Form.Check
+                type="radio"
+                name="basic-question"
+                onChange={updateResponse}
+                id="response-"
+                label=" "
+                value=" "
+                checked={response === " "}
+            />
+            </div>
         </div>
     );
 }
