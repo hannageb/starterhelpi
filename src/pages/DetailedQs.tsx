0857543be14d1fb88ccaf44041d29972f371fc02
Luc De Nardi <denardiluc@gmail.com>
2025-03-31 13:00:56 -0400

    updated to home page look-luc branch

--- src/pages/DetailedQs.tsx
+++ src/pages/DetailedQs.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import GoHomeScreen from './BasicQs';
+import { Form } from "react-bootstrap";
 
 function DetailedQ(){
+    const [response, setResponse] = useState<string>("");
+
+    function updateResponse(event: React.ChangeEvent<HTMLInputElement>) {
+        setResponse(event.target.value);
+    }
     return(
         <div>
-            <GoHomeScreen/>
+        <GoHomeScreen></GoHomeScreen>
+        <div>
+            <h3 style={{alignItems:'center'}}>
+                Question filler    
+            </h3>
+            <Form.Check
+                type="radio"
+                name="basic-question"
+                onChange={updateResponse}
+                id="response-"
+                label=" "
+                value=" "
+                checked={response === " "}
+            />
+            </div>
         </div>
-    );
+    )
 }
 export default DetailedQ
