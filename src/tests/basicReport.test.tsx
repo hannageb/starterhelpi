bf6645aebde1d83e6be7ed4798dd945413233a5d
Hanna <hannageb@udel.edu>
2025-04-27 20:49:11 -0400

    changed favicon

--- src/tests/basicReport.test.tsx
+++ src/tests/basicReport.test.tsx
@@ -5,5 +5,4 @@ test('renders envelope', () => {
   render(<BasicReport />);
   const linkElement = screen.getByRole("paragraph", {name: /story/i});
   expect(linkElement).toBeInTheDocument();
-  const responses = render.g
 });
